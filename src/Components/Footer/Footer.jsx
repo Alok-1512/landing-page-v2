12228da38fc713a2a871dc9850cc52e9cd529536
Alok Singh <alok15120803@gmail.com>
2023-05-14 01:39:09 +0530

    footer mob added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,84 +2,88 @@ import { tele } from '../../assets';
 import { whatsapp } from '../../assets';
 import { AppleStore, GetHire, PlayStore } from '../../assets';
 import './Footer.css';
+import FooterMobile from './FooterMobile';
 
 const Footer = () => {
   return (
-    <footer>
-      <div className='footer-main flex justify-evenly py-12 px-6'>
-        <div className='brand-cta space-y-5'>
-          <img src={GetHire} alt='' className='w-[150px]' />
-          <div className='download-cta flex'>
-            <span className='border-r-4 w-[45%]'> Download The App </span>
-            <div className='flex items-center justify-evenly w-[40%]'>
-              <img src={AppleStore} alt='' className='h-8' />
-              <img src={PlayStore} alt='' className='h-8' />
+    <>
+      <FooterMobile />
+      <footer>
+        <div className='footer-main flex justify-evenly py-12 px-6'>
+          <div className='brand-cta space-y-5'>
+            <img src={GetHire} alt='' className='w-[150px]' />
+            <div className='download-cta flex'>
+              <span className='border-r-4 w-[45%]'> Download The App </span>
+              <div className='flex items-center justify-evenly w-[40%]'>
+                <img src={AppleStore} alt='' className='h-8' />
+                <img src={PlayStore} alt='' className='h-8' />
+              </div>
+            </div>
+            <div className='download-cta flex'>
+              <span className='border-r-4 w-[45%]'>
+                {' '}
+                Follow Us On Social Media
+              </span>
+              <div className='flex items-center justify-evenly w-[40%]'>
+                <img src={whatsapp} alt='' className='h-8' />
+                <img src={tele} alt='' className='h-8' />
+              </div>
             </div>
           </div>
-          <div className='download-cta flex'>
-            <span className='border-r-4 w-[45%]'>
-              {' '}
-              Follow Us On Social Media
-            </span>
-            <div className='flex items-center justify-evenly w-[40%]'>
-              <img src={whatsapp} alt='' className='h-8' />
-              <img src={tele} alt='' className='h-8' />
-            </div>
+          <div className='footer-links'>
+            <p>Company:</p>
+            <ul>
+              <li>About Us</li>
+              <li>Our Team</li>
+              <li>Career</li>
+              <li>News</li>
+              <li>Media Kit</li>
+              <li>Contact</li>
+              <li>Site Map</li>
+            </ul>
+          </div>
+          <div className='footer-links'>
+            <p>Services:</p>
+            <ul>
+              <li>Recruiter</li>
+              <li>Talent</li>
+              <li>Colleges</li>
+            </ul>
+          </div>
+          <div className='footer-links'>
+            <p>Solutions:</p>
+            <ul>
+              <li>Post Job</li>
+              <li>Search Job</li>
+              <li>Consulting</li>
+            </ul>
+          </div>
+          <div className='footer-links'>
+            <p>Resources:</p>
+            <ul>
+              <li>Post Job</li>
+              <li>Search Job</li>
+              <li>Consulting</li>
+            </ul>
+          </div>
+          <div className='footer-links'>
+            <p>Policies:</p>
+            <ul>
+              <li>T&C</li>
+              <li>Privacy</li>
+              <li>Users</li>
+              <li>Greivances</li>
+              <li>Cookies</li>
+              <li>Data</li>
+              <li>Refund</li>
+            </ul>
           </div>
         </div>
-        <div className='footer-links'>
-          <p>Company:</p>
-          <ul>
-            <li>About Us</li>
-            <li>Our Team</li>
-            <li>Career</li>
-            <li>News</li>
-            <li>Media Kit</li>
-            <li>Contact</li>
-            <li>Site Map</li>
-          </ul>
+        <div className='footer-bottom px-6 py-2'>
+          <p>Made with ❤️ in India </p>
         </div>
-        <div className='footer-links'>
-          <p>Services:</p>
-          <ul>
-            <li>Recruiter</li>
-            <li>Talent</li>
-            <li>Colleges</li>
-          </ul>
-        </div>
-        <div className='footer-links'>
-          <p>Solutions:</p>
-          <ul>
-            <li>Post Job</li>
-            <li>Search Job</li>
-            <li>Consulting</li>
-          </ul>
-        </div>
-        <div className='footer-links'>
-          <p>Resources:</p>
-          <ul>
-            <li>Post Job</li>
-            <li>Search Job</li>
-            <li>Consulting</li>
-          </ul>
-        </div>
-        <div className='footer-links'>
-          <p>Policies:</p>
-          <ul>
-            <li>T&C</li>
-            <li>Privacy</li>
-            <li>Users</li>
-            <li>Greivances</li>
-            <li>Cookies</li>
-            <li>Data</li>
-            <li>Refund</li>
-          </ul>
-        </div>
-      </div>
-      <div className='footer-bottom px-6 py-2'>
-        <p>Made with ❤️ in India </p>
-      </div>
-    </footer>
+      </footer>
+    </>
   );
 };
 
---
+++ src/Components/Footer/FooterMobile.jsx
@@ -0,0 +1,48 @@
+import { tele, whatsapp, linkedin, GetHire } from '../../assets';
+import './Footer.css';
+import { Link } from 'react-router-dom';
+
+const FooterMobile = () => {
+  return (
+    <div className='mob_footer'>
+      <div className='img-logo'>
+        <img src={GetHire} alt='' className='h-[50px]' />
+      </div>
+      <div className='ham-menu-items'>
+        <ul>
+          <Link to='/contact'>
+            <li>Contact Us</li>
+          </Link>
+          <Link to='/about-us'>
+            <li>About Us</li>
+          </Link>
+          <Link to='/blog'>
+            <li>Blogs</li>
+          </Link>
+          <Link to='/teams'>
+            <li>Our Team</li>
+          </Link>
+        </ul>
+      </div>
+      <div>
+        <p className='text-center my-2 items-center'>Follows Us:</p>
+        <div className='icon-name flex justify-center gap-[10px]'>
+          <img src={tele} alt='' className='h-[35px]' />
+          <img src={whatsapp} alt='' className='h-[35px]' />
+          <img src={linkedin} alt='' className='h-[35px]' />
+          <img src={linkedin} alt='' className='h-[35px]' />
+          <img src={linkedin} alt='' className='h-[35px]' />
+        </div>
+        <div className='flex justify-center'>
+          <a href='' className='mx-auto'>
+            <button className='download_button  text-white my-5 mx-auto'>
+              Download App
+            </button>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FooterMobile;
